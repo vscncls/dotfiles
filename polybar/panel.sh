1851fdff5d6da88b9cbf96b6f06540efd979fbb6
Lucas Vasconcelos <47672257+vscncls@users.noreply.github.com>
2020-03-07 19:23:27 -0300

    remove unused variables

--- polybar/panel.sh
+++ polybar/panel.sh
@@ -4,7 +4,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-		WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-bspwm &
+		MONITOR=$m polybar --reload mainbar-bspwm &
 done
-
-echo "Bars launched..."
