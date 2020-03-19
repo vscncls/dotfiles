2d13a21fd460fe3ad6ea175ce020b1fbe3a39fad
Lucas Vasconcelos <lucaslou4@gmail.com>
2020-03-19 00:52:19 -0300

    updated polybar config

--- polybar/panel.sh
+++ polybar/panel.sh
@@ -3,6 +3,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-		MONITOR=$m polybar --reload mainbar-bspwm &
+PRIMARY=$(xrandr | grep primary | awk '{print $1}')
+
+MONITOR=$PRIMARY polybar --reload mainbar-bspwm &
+
+for m in $(polybar --list-monitors | cut -d":" -f1 | grep -v $PRIMARY); do
+		MONITOR=$m polybar --reload sidebar-bspwm &
 done
