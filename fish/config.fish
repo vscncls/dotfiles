c0156288b7e48f9e04c634da70e101e9e53ced85
Lucas Vasconcelos <lucaslou4@protonmail.com>
2020-05-12 19:55:40 -0300

    Add abbreviations and commom variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,24 @@
 set PATH (yarn global bin) $PATH
 set fish_greeting
+set -Ux PAGER less
+set -Ux EDITOR nvim
+set -Ux BROWSER firefox-developer-edition
+
+abbr g git
+abbr ga git add
+abbr ggl git pull
+abbr gfa git fetch --all --prune
+abbr gf git fetch
+abbr gd git diff
+abbr gst git status
+abbr gsh git show
+abbr ga git add
+abbr ga git add
+abbr gco git checkout
+abbr glg git log
+abbr gp git push
+abbr gca git commit --all -v
+abbr gca! git commit --all -v --amend
+abbr gc git commit -v
+abbr gc! git commit -v --amend
+abbr l ls
