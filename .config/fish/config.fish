7cd3b78eca9c020c9ad686c0419a98a6629aa1c4
Lucas Vasconcelos <lucaslou4@protonmail.com>
2020-08-06 12:13:25 -0300

    update dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set PATH (yarn global bin) $PATH
+set PATH ~/.emacs.d/bin $PATH
 set fish_greeting
 set -Ux PAGER less
 set -Ux EDITOR nvim
@@ -15,10 +16,20 @@ abbr gsh git show
 abbr ga git add
 abbr ga git add
 abbr gco git checkout
-abbr glg git log
+abbr glg git log --oneline
 abbr gp git push
 abbr gca git commit --all -v
 abbr gca! git commit --all -v --amend
 abbr gc git commit -v
 abbr gc! git commit -v --amend
 abbr l ls
+abbr gaa git add .
+
+abbr yw yarn workspace 
+abbr ya yarn add 
+abbr yad yarn add --dev 
+abbr yr yarn run
+
+set MAKEFLAGS '-j'(nproc)''
+
+source ~/.asdf/asdf.fish
