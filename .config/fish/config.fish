4d04f9567ebae9807397eb4a928adf2a9bcfc0d5
Lucas Vasconcelos <lucaslou4@protonmail.com>
2020-08-10 11:09:36 -0300

    add abbr to push git tags

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,7 @@ abbr ga git add
 abbr gco git checkout
 abbr glg git log --oneline
 abbr gp git push
+abbr gpt git push --tags
 abbr gca git commit --all -v
 abbr gca! git commit --all -v --amend
 abbr gc git commit -v
