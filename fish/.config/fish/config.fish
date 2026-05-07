5be31496ed9533354525640f753445c226f94939
lucas <lucaslou4@gmail.com>
2026-05-07 13:23:33 -0300

    x

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ export PATH="$HOME/.local/bin:$PATH"
 
 if status is-interactive
     set -U fish_greeting
+    set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
     fish_vi_key_bindings
 
     source $__fish_config_dir/abbr.fish
